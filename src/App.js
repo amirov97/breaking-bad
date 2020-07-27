61c3e300dd7035d9ecfc80bc020f4256b55fc43a
amirov <apic0098@gmail.com>
2020-07-28 01:00:05 +0430

    added characters grid and fetch items from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Header from "./components/header/header";
+import CharactersGrid from "./components/characters/charactersGrid";
 
 
 const App = ()=> {
+    const [items , setItems] = useState([])
+    const [isLoading , setIsLoading] = useState(true)
+
+    useEffect(()=>{
+         axios.get(`https://www.breakingbadapi.com/api/characters`)
+             .then(results=>{
+                 console.log(results.data)
+                 setItems(results.data)
+                 setIsLoading(false)
+             })
+    },[])
+
   return (
     <div className="container">
-      hello
+      <Header />
+      <CharactersGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
